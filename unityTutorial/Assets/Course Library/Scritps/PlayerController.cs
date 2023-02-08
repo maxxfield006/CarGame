b48337c08a84969c0488f7ffa2ef827d1395f57f
Max Field <maxfield7766@gmail.com>
2023-02-08 22:20:21 +1300

    Proof of Car game

---
+++ unityTutorial/Assets/Course Library/Scritps/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 20f;
+    public float turnSpeed;
+    public float horizontalInput;
+    public float verticalMovement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalMovement = Input.GetAxis("Vertical");
+        
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalMovement);
+        transform.Rotate(Vector3.up * turnSpeed * horizontalInput * Time.deltaTime);
+    }
+}
